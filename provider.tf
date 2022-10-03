0142cdca8f8a35bad8170bad856ce5919f049dd4
glennbech <glenn.bech@gmail.com>
2022-10-03 22:04:03 +0000

    provider2

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "pgr301-2021-terraform-state"
-    key    = "glennbech/apprunner.state"
+    key    = "glenn.richard.bech/apprunner.state"
     region = "eu-north-1"
   }
 }
